6d74e0fa7432eef519245a02e0bb38f84bca30a7
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-12-05 11:49:05 -0600

    cleaned up code. reorganized functions and variables

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 $(function () {
+    //VARIABLES
     //MAPBOX API
     mapboxgl.accessToken = MAP_BOX_KEY
     const map = new mapboxgl.Map({
@@ -11,28 +12,32 @@ $(function () {
         center: [-98.4861, 29.4260], // starting position [lng, lat]
         zoom: 7.15 // starting zoom
     });
+    // Mapbox Marker
+    const marker = new mapboxgl.Marker({
+        color: '#006AFF',
+        draggable: true
+    })
+        .setLngLat([-98.4861, 29.4260])
+        .addTo(map);
+    //Mapbox geocoder/search
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: false
+    });
 
     map.on('load', function () {
-        // Mapbox Marker
-        const marker = new mapboxgl.Marker({
-            color: '#006AFF',
-            draggable: true
-        })
-            .setLngLat([-98.4861, 29.4260])
-            .addTo(map);
+        //get weather on page load
+        getCurrentWeather();
+        getWeather();
 
-        //Mapbox geocoder/search
-        const geocoder = new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl,
-            marker: false
-        });
-
+        //CONTROLS
         map.addControl( //geocoder | search bar
             geocoder
         );
         map.addControl(new mapboxgl.NavigationControl()); //Navigation '+' & '-' zoom buttons
 
+        //EVENTS
         map.on('click', function (e) {
             marker.setLngLat(e.lngLat);
             map.flyTo({
@@ -66,95 +71,94 @@ $(function () {
             getCurrentWeather();
             getWeather();
         })
-        //get weather on page load
-        getCurrentWeather();
-        getWeather();
-
-        function getCurrentWeather() {
-            $.get("http://api.openweathermap.org/data/2.5/weather", {
-                lat: onDragEnd(1),
-                lon: onDragEnd(0),
-                units: 'imperial',
-                APPID: OPEN_WEATHER_MAP_KEY
-            }).done(function (data) {
-                let weatherIcon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
-                //get time
-                $("#timer").html(`Weather Data last accessed: ${changeToTimezone(data.dt, data.timezone)}`);
-                //get date
-                $("#date-current").html(convertDate(data.dt))
-                //get temp
-                $("#temp-current").html(data.main.temp.toFixed(1) + '&#8457')
-                //get description
-                $("#desc-current").html(`<strong>${capitalizeFirstLetter(data.weather[0].description)}</strong><img src=${weatherIcon} style="height: 50px; width: 50px; position: relative">`);
-                //get humidity
-                $("#hum-current").html(`Humidity: <strong>${data.main.humidity}%</strong>`);
-                //get wind
-                $("#wind-current").html(`Wind: <strong>${data.wind.speed}</strong> mph`);
-                //get pressure
-                $("#pres-current").html(`Barometer: <strong>${baroPressure(data.main.pressure).toFixed(2)}</strong> inHg`);
-            });
-        }
-
-        //OPENWEATHER API
-        function getWeather() {
-            $.get("http://api.openweathermap.org/data/2.5/forecast", {
-                lat: onDragEnd(1),
-                lon: onDragEnd(0),
-                units: "imperial",
-                APPID: OPEN_WEATHER_MAP_KEY
-            }).done(function (data) {
-                function weatherIndex(i) {
-                    return "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png"
-                }
-                //get city
-                $("#city").html(`Current City: ${data.city.name}`);
-                //get dates
-                $("#date-1").html(convertDate(data.list[0].dt));
-                $("#date-2").html(convertDate(data.list[8].dt));
-                $("#date-3").html(convertDate(data.list[16].dt));
-                $("#date-4").html(convertDate(data.list[24].dt));
-                $("#date-5").html(convertDate(data.list[32].dt));
-                //get temps
-                $("#temp-1").html(`<strong>${data.list[0].main.temp.toFixed(1)}</strong>&#8457`);
-                $("#temp-2").html(`<strong>${data.list[8].main.temp.toFixed(1)}</strong>&#8457`);
-                $("#temp-3").html(`<strong>${data.list[16].main.temp.toFixed(1)}</strong>&#8457`);
-                $("#temp-4").html(`<strong>${data.list[24].main.temp.toFixed(1)}</strong>&#8457`);
-                $("#temp-5").html(`<strong>${data.list[32].main.temp.toFixed(1)}</strong>&#8457`);
-                //get description
-                $("#desc-1").html(`<strong>${capitalizeFirstLetter(data.list[0].weather[0].description)}</strong><img src=${weatherIndex(0)} style="height: 50px; width: 50px">`);
-                $("#desc-2").html(`<strong>${capitalizeFirstLetter(data.list[8].weather[0].description)}</strong><img src=${weatherIndex(8)} style="height: 50px; width: 50px">`);
-                $("#desc-3").html(`<strong>${capitalizeFirstLetter(data.list[16].weather[0].description)}</strong><img src=${weatherIndex(16)} style="height: 50px; width: 50px">`);
-                $("#desc-4").html(`<strong>${capitalizeFirstLetter(data.list[24].weather[0].description)}</strong><img src=${weatherIndex(24)} style="height: 50px; width: 50px">`);
-                $("#desc-5").html(`<strong>${capitalizeFirstLetter(data.list[32].weather[0].description)}</strong><img src=${weatherIndex(32)} style="height: 50px; width: 50px">`);
-                //get humidity
-                $("#hum-1").html(`Humidity: <strong>${data.list[0].main.humidity}%</strong>`);
-                $("#hum-2").html(`Humidity: <strong>${data.list[8].main.humidity}%</strong>`);
-                $("#hum-3").html(`Humidity: <strong>${data.list[16].main.humidity}%</strong>`);
-                $("#hum-4").html(`Humidity: <strong>${data.list[24].main.humidity}%</strong>`);
-                $("#hum-5").html(`Humidity: <strong>${data.list[32].main.humidity}%</strong>`);
-                //get wind
-                $("#wind-1").html(`Wind: <strong>${data.list[0].wind.speed}</strong> mph`);
-                $("#wind-2").html(`Wind: <strong>${data.list[8].wind.speed}</strong> mph`);
-                $("#wind-3").html(`Wind: <strong>${data.list[16].wind.speed}</strong> mph`);
-                $("#wind-4").html(`Wind: <strong>${data.list[24].wind.speed}</strong> mph`);
-                $("#wind-5").html(`Wind: <strong>${data.list[32].wind.speed}</strong> mph`);
-                //get pressure
-                $("#pres-1").html(`Barometer: <strong>${baroPressure(data.list[0].main.pressure).toFixed(2)}</strong> inHg`);
-                $("#pres-2").html(`Barometer: <strong>${baroPressure(data.list[8].main.pressure).toFixed(2)}</strong> inHg`);
-                $("#pres-3").html(`Barometer: <strong>${baroPressure(data.list[16].main.pressure).toFixed(2)}</strong> inHg`);
-                $("#pres-4").html(`Barometer: <strong>${baroPressure(data.list[24].main.pressure).toFixed(2)}</strong> inHg`);
-                $("#pres-5").html(`Barometer: <strong>${baroPressure(data.list[32].main.pressure).toFixed(2)}</strong> inHg`);
-            });
-        }
-
-        function onDragEnd(i) {
-            const lngLat = marker.getLngLat();
-            let arr = []
-            arr.push(lngLat.lng, lngLat.lat)
-            return arr[i]
-        }
     });
 
+    //gets coordinates
+    function onDragEnd(i) {
+        const lngLat = marker.getLngLat();
+        let arr = []
+        arr.push(lngLat.lng, lngLat.lat)
+        return arr[i]
+    }
+
+    //OPENWEATHER API
+    function getCurrentWeather() {
+        $.get("http://api.openweathermap.org/data/2.5/weather", {
+            lat: onDragEnd(1),
+            lon: onDragEnd(0),
+            units: 'imperial',
+            APPID: OPEN_WEATHER_MAP_KEY
+        }).done(function (data) {
+            let weatherIcon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+            //get time
+            $("#timer").html(`Weather Data last accessed: ${changeToTimezone(data.dt, data.timezone)}`);
+            //get date
+            $("#date-current").html(convertDate(data.dt))
+            //get temp
+            $("#temp-current").html(data.main.temp.toFixed(1) + '&#8457')
+            //get description
+            $("#desc-current").html(`<strong>${capitalizeFirstLetter(data.weather[0].description)}</strong><img src=${weatherIcon} style="height: 50px; width: 50px; position: relative">`);
+            //get humidity
+            $("#hum-current").html(`Humidity: <strong>${data.main.humidity}%</strong>`);
+            //get wind
+            $("#wind-current").html(`Wind: <strong>${data.wind.speed}</strong> mph`);
+            //get pressure
+            $("#pres-current").html(`Barometer: <strong>${baroPressure(data.main.pressure).toFixed(2)}</strong> inHg`);
+        });
+    }
+
+
+    //FUNCTIONS
+    function getWeather() {
+        $.get("http://api.openweathermap.org/data/2.5/forecast", {
+            lat: onDragEnd(1),
+            lon: onDragEnd(0),
+            units: "imperial",
+            APPID: OPEN_WEATHER_MAP_KEY
+        }).done(function (data) {
+            function weatherIndex(i) {
+                return "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png"
+            }
+            //get city
+            $("#city").html(`Current City: ${data.city.name}`);
+            //get dates
+            $("#date-1").html(convertDate(data.list[0].dt));
+            $("#date-2").html(convertDate(data.list[8].dt));
+            $("#date-3").html(convertDate(data.list[16].dt));
+            $("#date-4").html(convertDate(data.list[24].dt));
+            $("#date-5").html(convertDate(data.list[32].dt));
+            //get temps
+            $("#temp-1").html(`<strong>${data.list[0].main.temp.toFixed(1)}</strong>&#8457`);
+            $("#temp-2").html(`<strong>${data.list[8].main.temp.toFixed(1)}</strong>&#8457`);
+            $("#temp-3").html(`<strong>${data.list[16].main.temp.toFixed(1)}</strong>&#8457`);
+            $("#temp-4").html(`<strong>${data.list[24].main.temp.toFixed(1)}</strong>&#8457`);
+            $("#temp-5").html(`<strong>${data.list[32].main.temp.toFixed(1)}</strong>&#8457`);
+            //get description
+            $("#desc-1").html(`<strong>${capitalizeFirstLetter(data.list[0].weather[0].description)}</strong><img src=${weatherIndex(0)} style="height: 50px; width: 50px">`);
+            $("#desc-2").html(`<strong>${capitalizeFirstLetter(data.list[8].weather[0].description)}</strong><img src=${weatherIndex(8)} style="height: 50px; width: 50px">`);
+            $("#desc-3").html(`<strong>${capitalizeFirstLetter(data.list[16].weather[0].description)}</strong><img src=${weatherIndex(16)} style="height: 50px; width: 50px">`);
+            $("#desc-4").html(`<strong>${capitalizeFirstLetter(data.list[24].weather[0].description)}</strong><img src=${weatherIndex(24)} style="height: 50px; width: 50px">`);
+            $("#desc-5").html(`<strong>${capitalizeFirstLetter(data.list[32].weather[0].description)}</strong><img src=${weatherIndex(32)} style="height: 50px; width: 50px">`);
+            //get humidity
+            $("#hum-1").html(`Humidity: <strong>${data.list[0].main.humidity}%</strong>`);
+            $("#hum-2").html(`Humidity: <strong>${data.list[8].main.humidity}%</strong>`);
+            $("#hum-3").html(`Humidity: <strong>${data.list[16].main.humidity}%</strong>`);
+            $("#hum-4").html(`Humidity: <strong>${data.list[24].main.humidity}%</strong>`);
+            $("#hum-5").html(`Humidity: <strong>${data.list[32].main.humidity}%</strong>`);
+            //get wind
+            $("#wind-1").html(`Wind: <strong>${data.list[0].wind.speed}</strong> mph`);
+            $("#wind-2").html(`Wind: <strong>${data.list[8].wind.speed}</strong> mph`);
+            $("#wind-3").html(`Wind: <strong>${data.list[16].wind.speed}</strong> mph`);
+            $("#wind-4").html(`Wind: <strong>${data.list[24].wind.speed}</strong> mph`);
+            $("#wind-5").html(`Wind: <strong>${data.list[32].wind.speed}</strong> mph`);
+            //get pressure
+            $("#pres-1").html(`Barometer: <strong>${baroPressure(data.list[0].main.pressure).toFixed(2)}</strong> inHg`);
+            $("#pres-2").html(`Barometer: <strong>${baroPressure(data.list[8].main.pressure).toFixed(2)}</strong> inHg`);
+            $("#pres-3").html(`Barometer: <strong>${baroPressure(data.list[16].main.pressure).toFixed(2)}</strong> inHg`);
+            $("#pres-4").html(`Barometer: <strong>${baroPressure(data.list[24].main.pressure).toFixed(2)}</strong> inHg`);
+            $("#pres-5").html(`Barometer: <strong>${baroPressure(data.list[32].main.pressure).toFixed(2)}</strong> inHg`);
+        });
+    }
     //convert hectopascals to inch of mercury | hPa -> inHg
     function baroPressure(pressure) {
         const inchOfMercuryPerHectopascal = 0.02952998597817832;
